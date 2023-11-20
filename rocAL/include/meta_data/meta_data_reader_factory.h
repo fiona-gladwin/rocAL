59939c79b61bde909b737390b2643548660c9962
fgladwin <fgladwin@amd.com>
2023-11-20 02:16:17 -0500

    Add API to return the metadata reader and output after creation

--- rocAL/include/meta_data/meta_data_reader_factory.h
+++ rocAL/include/meta_data/meta_data_reader_factory.h
@@ -23,3 +23,5 @@ THE SOFTWARE.
 #pragma once
 #include "meta_data_reader.h"
 std::shared_ptr<MetaDataReader> create_meta_data_reader(const MetaDataConfig& config, pMetaDataBatch& meta_data_batch);
+std::pair<std::shared_ptr<MetaDataReader>, pMetaDataBatch> create_meta_data_reader(const MetaDataConfig& config);
+

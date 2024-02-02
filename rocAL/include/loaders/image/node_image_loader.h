e1b5fcde25b1f2720b339fe135ca482115f1c959
fiona-gladwin <fionagladwin@multicorewareinc.com>
2024-02-02 03:53:34 -0500

    Add TODO comments

--- rocAL/include/loaders/image/node_image_loader.h
+++ rocAL/include/loaders/image/node_image_loader.h
@@ -41,6 +41,8 @@ class ImageLoaderNode : public Node {
     /// for example if there are 10 images in the dataset and load_batch_count is 3, the loader repeats 2 images as if there are 12 images available.
     void init(unsigned internal_shard_count, unsigned cpu_num_threads, const std::string &source_path, const std::string &json_path, const std::map<std::string, std::string> feature_key_map, StorageType storage_type, DecoderType decoder_type, bool shuffle, bool loop,
               size_t load_batch_count, RocalMemType mem_type, std::shared_ptr<MetaDataReader> meta_data_reader, bool decoder_keep_orig = false, const char *prefix = "", unsigned sequence_length = 0, unsigned step = 0, unsigned stride = 0);
+    
+    // TODO - Will replace existing init function once all readers and decoders are changed
     void init(Tensor *jpegs, unsigned internal_shard_count, unsigned cpu_num_threads, DecoderType decoder_type, ReaderConfig reader_cfg,
               size_t load_batch_count, RocalMemType mem_type, bool decoder_keep_orig = false, const char *file_prefix = "");
     std::shared_ptr<LoaderModule> get_loader_module();

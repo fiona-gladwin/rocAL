9d1e10664c397f06044281fb881ef3dbc5b6f921
fiona-gladwin <fionagladwin@multicorewareinc.com>
2024-02-02 03:27:49 -0500

    Add description

--- rocAL/include/api/rocal_api_data_loaders.h
+++ rocAL/include/api/rocal_api_data_loaders.h
@@ -32,30 +32,61 @@ THE SOFTWARE.
  * \brief The AMD rocAL data loader functions.
  */
 
-extern "C" RocalTensor ROCAL_API_CALL rocalImageDecoder(
-    RocalContext p_context,
-    RocalTensor p_jpegs,
-    RocalImageColor rocal_color_format,
-    unsigned internal_shard_count,
-    bool is_output = false,
-    bool shuffle = false,
-    bool loop = false,
-    RocalImageSizeEvaluationPolicy decode_size_policy = ROCAL_USE_MOST_FREQUENT_SIZE,
-    unsigned max_width = 0, unsigned max_height = 0,
-    RocalDecoderType rocal_decoder_type = RocalDecoderType::ROCAL_DECODER_TJPEG);
+/*! \brief Creates image reader and decoder. It allocates the resources and objects required to read and decode Jpeg images. It has internal sharding capability to load/decode in parallel is user wants. If images are not Jpeg compressed they will be ignored.
+ * \ingroup group_rocal_data_loaders
+ * \param [in] context Rocal context
+ * \param [in] jpegs Readers output
+ * \param [in] source_path A NULL terminated char string pointing to the location on the disk
+ * \param [in] rocal_color_format The color format the images will be decoded to.
+ * \param [in] internal_shard_count Defines the parallelism level by internally sharding the input dataset and load/decode using multiple decoder/loader instances. Using shard counts bigger than 1 improves the load/decode performance if compute resources (CPU cores) are available.
+ * \param [in] is_output Determines if the user wants the loaded tensors to be part of the output or not.
+ * \param [in] shuffle Determines if the user wants to shuffle the dataset or not.
+ * \param [in] loop Determines if the user wants to indefinitely loops through images or not.
+ * \param [in] decode_size_policy
+ * \param [in] max_width The maximum width of the decoded images, larger or smaller will be resized to closest
+ * \param [in] max_height The maximum height of the decoded images, larger or smaller will be resized to closest
+ * \param [in] rocal_decoder_type Determines the decoder_type, tjpeg or hwdec
+ * \return Reference to the output tensor
+ */
+extern "C" RocalTensor ROCAL_API_CALL rocalImageDecoder(RocalContext p_context,
+                                                        RocalTensor p_jpegs,
+                                                        RocalImageColor rocal_color_format,
+                                                        unsigned internal_shard_count,
+                                                        bool is_output = false,
+                                                        bool shuffle = false,
+                                                        bool loop = false,
+                                                        RocalImageSizeEvaluationPolicy decode_size_policy = ROCAL_USE_MOST_FREQUENT_SIZE,
+                                                        unsigned max_width = 0, unsigned max_height = 0,
+                                                        RocalDecoderType rocal_decoder_type = RocalDecoderType::ROCAL_DECODER_TJPEG);
 
-extern "C" RocalTensor ROCAL_API_CALL rocalImageDecoderSingleShard(
-    RocalContext p_context,
-    RocalTensor p_jpegs,
-    RocalImageColor rocal_color_format,
-    unsigned shard_id,
-    unsigned shard_count,
-    bool is_output = false,
-    bool shuffle = false,
-    bool loop = false,
-    RocalImageSizeEvaluationPolicy decode_size_policy = ROCAL_USE_MOST_FREQUENT_SIZE,
-    unsigned max_width = 0, unsigned max_height = 0,
-    RocalDecoderType rocal_decoder_type = RocalDecoderType::ROCAL_DECODER_TJPEG);
+/*! \brief Creates image reader and decoder. It allocates the resources and objects required to read and decode Jpeg images. It has internal sharding capability to load/decode in parallel is user wants. If images are not Jpeg compressed they will be ignored.
+ * \ingroup group_rocal_data_loaders
+ * \param [in] context Rocal context
+ * \param [in] jpegs Readers output
+ * \param [in] source_path A NULL terminated char string pointing to the location on the disk
+ * \param [in] rocal_color_format The color format the images will be decoded to.
+ * \param [in] shard_id Shard id for this loader
+ * \param [in] shard_count Total shard count
+ * \param [in] is_output Determines if the user wants the loaded tensors to be part of the output or not.
+ * \param [in] shuffle Determines if the user wants to shuffle the dataset or not.
+ * \param [in] loop Determines if the user wants to indefinitely loops through images or not.
+ * \param [in] decode_size_policy
+ * \param [in] max_width The maximum width of the decoded images, larger or smaller will be resized to closest
+ * \param [in] max_height The maximum height of the decoded images, larger or smaller will be resized to closest
+ * \param [in] rocal_decoder_type Determines the decoder_type, tjpeg or hwdec
+ * \return Reference to the output tensor
+ */
+extern "C" RocalTensor ROCAL_API_CALL rocalImageDecoderSingleShard(RocalContext p_context,
+                                                                   RocalTensor p_jpegs,
+                                                                   RocalImageColor rocal_color_format,
+                                                                   unsigned shard_id,
+                                                                   unsigned shard_count,
+                                                                   bool is_output = false,
+                                                                   bool shuffle = false,
+                                                                   bool loop = false,
+                                                                   RocalImageSizeEvaluationPolicy decode_size_policy = ROCAL_USE_MOST_FREQUENT_SIZE,
+                                                                   unsigned max_width = 0, unsigned max_height = 0,
+                                                                   RocalDecoderType rocal_decoder_type = RocalDecoderType::ROCAL_DECODER_TJPEG);
 
 /*! \brief Creates JPEG image reader and decoder. It allocates the resources and objects required to read and decode Jpeg images stored on the file systems. It has internal sharding capability to load/decode in parallel is user wants. If images are not Jpeg compressed they will be ignored.
  * \ingroup group_rocal_data_loaders

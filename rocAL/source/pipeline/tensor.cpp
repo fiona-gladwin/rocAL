6f977d48ec067adbdbe621e6ff1d072903d334eb
fiona-gladwin <fionagladwin@multicorewareinc.com>
2025-03-06 13:24:02 +0000

    Introduce tensor name member to Tensor class

--- rocAL/source/pipeline/tensor.cpp
+++ rocAL/source/pipeline/tensor.cpp
@@ -283,6 +283,7 @@ Tensor::~Tensor() {
 Tensor::Tensor(const TensorInfo &tensor_info)
     : _info(tensor_info) {
     _info._type = TensorInfo::Type::UNKNOWN;
+    _tensor_name = "tensor_" + std::to_string(_tensor_idx++);
     _mem_handle = nullptr;
 }
 

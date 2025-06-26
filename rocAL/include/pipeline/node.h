d85383288d48d74f89a81374dc8f4e00736fc613
fiona-gladwin <fionagladwin@multicorewareinc.com>
2025-06-26 10:20:02 +0000

    Fix args list in ImageLoaderNode

--- rocAL/include/pipeline/node.h
+++ rocAL/include/pipeline/node.h
@@ -95,6 +95,7 @@ class Argument {
         {typeid(RocalBatchPolicy), "RocalBatchPolicy"},
         {typeid(RocalResizeInterpolationType), "RocalResizeInterpolationType"},
         {typeid(RocalResizeScalingMode), "RocalResizeScalingMode"},
+        {typeid(RocalMemType), "RocalMemType"},
     };
 
     template <typename T>

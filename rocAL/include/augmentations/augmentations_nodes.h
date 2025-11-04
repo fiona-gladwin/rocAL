91aa867a02feee32b51efc0f1116f02ebfaa32d1
fiona-gladwin <fionagladwin@multicorewareinc.com>
2025-11-04 06:07:48 -0600

    Add support for remap augmentation

--- rocAL/include/augmentations/augmentations_nodes.h
+++ rocAL/include/augmentations/augmentations_nodes.h
@@ -82,3 +82,4 @@ THE SOFTWARE.
 #include "augmentations/filter_augmentations/node_magnitude.h"
 #include "augmentations/filter_augmentations/node_phase.h"
 #include "augmentations/geometry_augmentations/node_crop_and_patch.h"
+#include "augmentations/geometry_augmentations/node_remap.h"
